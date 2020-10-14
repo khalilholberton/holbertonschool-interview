262280ffe8ac5fb5be25258810d416957aa9e890
khalilholberton <1071@holbertonschool.com>
2020-10-14 17:32:01 +0000

    fix

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * check_cycle - verify if a  singly linked list has a cycle
+ * @list: pointer
+ * Return: 1 if list has a cycle, 0 otherwise.
+ **/
+
+int check_cycle(listint_t *list)
+{
+	listint_t *a;
+	listint_t *b;
+
+	b = list->next->next;
+	a = list->next;
+
+	if (list->next == NULL)
+	{
+		return (0);
+	}
+	if (list == NULL)
+	{
+		return (0);
+	}
+
+	while (a && b  && b->next)
+	{
+		if (a == b->next)
+			return (1);
+		a = a->next;
+		b = b->next->next;
+	}
+	return (0);
+}
