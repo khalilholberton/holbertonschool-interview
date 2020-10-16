ecab0a2a7abda3d69e8d86b23deaeeb685f01491
khalilholberton <1071@holbertonschool.com>
2020-10-16 14:58:45 +0000

    fix

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -8,16 +8,27 @@ def canUnlockAll(boxes):
     """
     Return True if all boxes can be opened, else return False
     """
-    if type(boxes) is not list:
-        return False
-    if len(boxes) == 0:
-        return False
-    keys = boxes[0]
-    for i in range(1, len(boxes)):
-        if i in keys:
-            keys += boxes[i]
-    for i in range(len(boxes)):
-        if i in keys:
+    try:
+        ky = boxes[0]
+        f_level = range(1, len(boxes))
+        for count in f_level:
+            if count in ky:
+                ky = ky + boxes[count]
+        try:
+            for count in range(1, len(boxes)):
+                if count in ky:
+                    ky = ky + boxes[count]
+                    list(set(ky))
+                    x = 1
+        except Exception:
+            pass
+        s_level = range(1, len(boxes)-1)
+        for count in s_level:
+            if count not in ky:
+                x = 0
+        if x == 1:
             return True
         else:
             return False
+    except Exception:
+        pass
