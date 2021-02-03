9808d182f36e07606939843e0a4a42e48fea68a8
khalilholberton <1071@holbertonschool.com>
2021-02-03 19:24:36 +0000

    fix

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,23 +1,15 @@
 #!/usr/bin/node
-
-const ar = process.argv[2];
-const reqU = require('util');
-const reqR = require('request');
-const request = reqU.promisify(reqR);
-
-async function starwars (idx) {
-  const req = await (await request('https://swapi-api.hbtn.io/api/films/' + idx)).body;
-  const obJSON = JSON.parse(req);
-  const charVar = obJSON.charVar;
-  let ctr = 0;
-
-  while (ctr < charVar.length) {
-    const space = charVar[ctr];
-    const info = await (await request(space)).body;
-    const d = JSON.parse(info);
-    console.log(d.name);
-    ctr++;
-  }
+const request = require('request');
+const arg = process.argv[2];
+const U = 'https://swapi-api.hbtn.io/api/films/' + arg;
+request(U, (e, tot, body) => {
+  if (!e) { charac(JSON.parse(body).characters, 0); }
+});
+function charac (path, i) {
+  request(path[i], (e, tot, body) => {
+    if (!e) {
+      console.log(JSON.parse(body).name);
+      if (i + 1 < path.length) charac(path, ++i);
+    }
+  });
 }
-
-starwars(ar[0]);
